f523ad94447dfd2c13e7ea083672cf060691171c
vaisane <t0vaja01@students.oamk.fi>
2021-11-25 13:25:37 +0200

    add restaurant

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -20,3 +20,11 @@ exports.getRestaurantById = (req, res) => {
         return res.json(result);
     })
 }
+
+exports.addRestaurant = (req, res) => {
+    if(req.body.name == "" || req.body.Address == "" || req.body.time == "" || req.body.type == "" || req.body.price == "" || req.body.image == "") res.send("Some fields are empty")
+    db.query(`INSERT into restaurant VALUES(null, "${req.body.name}","${req.body.Address}","${req.body.time}","${req.body.type}","${req.body.price}","${req.body.image}",1)`, (error, result) => {
+        if(error) console.log(error)
+        return res.send("Restaurant added");
+    })
+}
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -4,5 +4,5 @@ const router = express.Router()
 
 router.get("/", restaurantsController.getAllRestaurants);
 router.get("/:id", restaurantsController.getRestaurantById);
-
+router.post("/", restaurantsController.addRestaurant);
 module.exports = router;
