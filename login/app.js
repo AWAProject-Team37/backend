67937ed39db6c8be017f5b537928d0af827b002b
Duong Pham <t0phtr00@students.oamk.fi>
2021-12-01 03:03:03 +0200

    Update

--- login/app.js
+++ login/app.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcrypt')
 const database = require('./config/database')
 const express = require('express')
 const flash = require('express-flash')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 
@@ -26,31 +27,32 @@ app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(flash())
+app.use(methodOverride('_method'))
+app.use(passport.initialize())
 app.use(session({
 	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: false
 }))
-app.use(passport.initialize())
 app.use(session())
 
 
 // RENDER PAGES
-app.get('/', (req, res) => {
+app.get('/', checkAuthenticated, (req, res) => {
 	res.render('index.ejs', { group : 'AWA-Project-37' })
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
 	res.render('login.ejs')
 })
 
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
 	res.render('register.ejs')
 })
 
 
 // LOGIN
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
 	successRedirect: '/',
 	failureRedirect: '/login',
 	failureFlash: true
@@ -58,7 +60,7 @@ app.post('/login', passport.authenticate('local', {
 
 
 // REGISTER
-app.post('/register', async (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
 	try {
 		const { firstName, lastName, email, password, passwordConfirm } = req.body
 
@@ -68,9 +70,9 @@ app.post('/register', async (req, res) => {
 				console.log(error)
 			}
 			if (result.length > 0) {
-				return
+				return res.redirect('/register', { message: 'Email has already be used!' })
 			} else if (password !== passwordConfirm) { // Password match check
-				return
+				return res.redirect('/register', { message: 'Passwords does not match!' })
 			}
 		})
 
@@ -94,6 +96,31 @@ app.post('/register', async (req, res) => {
 	}
 })
 
-const port = 3000
+
+// LOGOUT
+app.delete('/logout', (req, res) => {
+	req.logOut()
+	res.redirect('/login')
+})
+
+
+// LOGIN CHECK
+function checkAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next()
+	}
+	res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/')
+	}
+	next()
+}
+
+
+// PORT
+const port = process.env.PORT || 3000
 app.listen(port)
 console.log('Server is running on port ' + port)
